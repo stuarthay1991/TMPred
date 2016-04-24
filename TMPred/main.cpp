015722555f19f73689c9136c8b235d340d0a75f3
Stuart Brendan Hay <haysb@Stuarts-MacBook-Pro.local>
2016-04-24 12:04:52 -0700

    Initial Commit

---
+++ TMPred/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TMPred
+//
+//  Created by Stuart Brendan Hay on 4/24/16.
+//  Copyright (c) 2016 None. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
